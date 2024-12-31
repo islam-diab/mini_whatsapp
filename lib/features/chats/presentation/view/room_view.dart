a5a39d70a368d2282d7939f553e883c71d1aae89
islam-diab <islamdiab304@gmail.com>
2024-12-31 18:36:35 +0200

    - Add Shimmer Loading

--- lib/features/chats/presentation/view/room_view.dart
+++ lib/features/chats/presentation/view/room_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mini_whatsapp/core/widget/app_floating_button.dart';
+import 'package:mini_whatsapp/features/chats/presentation/view/widget/chats_shimmer_loading.dart';
 import 'package:mini_whatsapp/features/chats/presentation/view/widget/show_all_user.dart';
 import 'package:mini_whatsapp/features/chats/presentation/view/widget/user_list_view.dart';
 import 'package:mini_whatsapp/features/chats/presentation/view_model/chats_cubit.dart';
@@ -12,13 +13,24 @@ class RoomView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Stack(
+    return Scaffold(
+      floatingActionButton: AppFloatingButton(
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              fullscreenDialog: true,
+              builder: (context) => const ShowAllUser(),
+            ),
+          );
+        },
+        icon: Icons.message,
+      ),
+      body: const Stack(
         children: [
           RoomBlocBuilder(
             isOnline: true,
           ),
-          ChatFloatingButton(),
+          // ChatFloatingButton(),
         ],
       ),
     );
